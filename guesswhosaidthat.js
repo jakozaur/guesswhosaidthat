989b7e24c48b1c2d94ad1d5232b34ca6ececf458
Jacek Migdal <jacek@migdal.pl>
2014-09-29 21:45:40 -0700

    Add edit people panel

--- guesswhosaidthat.js
+++ guesswhosaidthat.js
@@ -12,9 +12,43 @@ if (Meteor.isClient) {
     }
   });
 
+
   Template.admin.people = function () {
     return People.find({});
-  }
+  };
+
+  Template.admin.editPerson = function () {
+    return People.findOne(Session.get('adminEditPerson'));
+  };
+
+  AutoForm.hooks({
+    updatePerson: {
+      onSuccess: function() {
+        Session.set('adminEditPerson', null);
+      }
+    }
+  });
+
+  AutoForm.addHooks(null, {
+    onSubmit: function () {
+      console.log("onSubmit ALL FORMS! 1");
+    },
+    onSuccess: function () {
+      console.log("onSubmit ALL FORMS! 2");
+
+    }
+  });
+
+  Session.setDefault('adminEditPerson', null);
+
+  Template.adminPerson.events({
+    'click .remove': function () {
+      People.remove(this._id);
+    },
+    'click .edit': function () {
+      Session.set('adminEditPerson', this._id);
+    }
+  });
 }
 
 if (Meteor.isServer) {
